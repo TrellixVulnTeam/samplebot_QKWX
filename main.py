22fbbb951a4f6c58012df0e086f8f73afa0ad838
Pavel Larionov <fuccpuff@gmail.com>
2020-12-27 14:01:55 +0500

    add keyboard

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import telebot
 
+keyboard = telebot.types.ReplyKeyboardMarkup()
+keyboard.row('Привет', 'Пока')
+
 bot = telebot.TeleBot("1492450911:AAGpYNKiCQc0T5WXFQmdB2nhTqarF-zMJB4")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Хеллоу хуман")
+    bot.send_message(message.chat.id, "Хеллоу хуман", reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
